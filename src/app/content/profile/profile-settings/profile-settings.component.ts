c7f35fb8d72e21dd1989c9053a32764f35b3b63d
Salil Chincholikar <chincholikarsalil@gmail.com>
2021-09-06 19:54:41 +0530

    updated ui to dark theme

--- src/app/content/profile/profile-settings/profile-settings.component.ts
+++ src/app/content/profile/profile-settings/profile-settings.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-profile-settings',
@@ -7,9 +10,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileSettingsComponent implements OnInit {
 
-  constructor() { }
+  imageSrc: string = '../../favicon.ico';
+  imageUpload = new FormGroup({
+    file: new FormControl('', [Validators.required]),
+    fileSource: new FormControl('', [Validators.required])
+  });
+
+  name = sessionStorage.getItem("name");
+  username = "@" + sessionStorage.getItem("username");
+  email = sessionStorage.getItem("email");
+  mobileNumber = "+91" + sessionStorage.getItem("mobileNumber");
+  dob = sessionStorage.getItem("dob");
+  joinedOn = sessionStorage.getItem("joinedOn");
+
+  editName = '';
+  editPhone = '';
+  editDob = '';
+  editEmail = '';
+
+  constructor(private http: HttpClient, private loginService: LoginService) {
+    if (!this.loginService.isLoggedIn()) {
+      window.location.href = "/login";
+    }
+  }
 
   ngOnInit(): void {
+  }
+
+  editProfile() {
+    window.location.href = "http://localhost:4200/profile/settings";
+  }
+
+  deleteUser() {
+    let delPwd = prompt("Confirm your password to delete your profile");
+    this.http.post<string>("http://localhost:8080/user/delete", JSON.stringify({ "username": sessionStorage.getItem("username"), "delPwd": delPwd })).subscribe(
+      data => console.log(data),
+      error => {
+        alert(error.error.text)
+        if (error.error.text == "User deleted!") {
+          window.sessionStorage.clear();
+          window.location.href = "/login";
+        }
+      }
+    );
+  }
+
+  uploadPhoto() {
+    console.log("upload");
+  }
+
+  get f() {
+    return this.imageUpload.controls;
+  }
+
+  onFileChange(event: any) {
+    const reader = new FileReader();
+
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+
+        this.imageSrc = reader.result as string;
+
+        this.imageUpload.patchValue({
+          fileSource: reader.result
+        });
+
+      };
+
+    }
+  }
+
+  submit() {
+    console.log(this.imageUpload.value);
+    this.http.post('http://localhost:8001/upload.php', this.imageUpload.value)
+      .subscribe(res => {
+        console.log(res);
+        alert('Uploaded Successfully.');
+      })
   }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -18,6 +18,7 @@ export class HeaderComponent implements OnInit {
   currentPage!: string;
 
   faUser = faUser;
+  username = sessionStorage.getItem("username");
 
   show: boolean = false;
 
